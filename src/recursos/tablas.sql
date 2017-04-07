e8c9110a5fa320723813be56bfc002494d7983ff
Steven Fernandez <valquiria98s@gmail.com>
2017-04-07 10:50:07 -0300

    SQL

--- src/recursos/tablas.sql
+++ src/recursos/tablas.sql
@@ -4,7 +4,31 @@ use productosVentas;
 create table electrodomesticos
 (id int not null primary key,
 artefactos_id int not null,
-carateristicas_id int not null,
+caracteristicas_id int not null,
+descripcion varchar(100) not null,
+FOREIGN KEY (artefacto_id) REFERENCES artefacto(id),
+FOREIGN KEY (caracteristica_id) REFERENCES caracteristica(id));
+
+create table televisores
+(id int not null primary key,
+artefactos_id int not null,
+caracteristicas_id int not null,
+descripcion varchar(100) not null,
+FOREIGN KEY (artefacto_id) REFERENCES artefacto(id),
+FOREIGN KEY (caracteristica_id) REFERENCES caracteristica(id));
+
+create table notebook
+(id int not null primary key,
+artefactos_id int not null,
+caracteristicas_id int not null,
+descripcion varchar(100) not null,
+FOREIGN KEY (artefacto_id) REFERENCES artefacto(id),
+FOREIGN KEY (caracteristica_id) REFERENCES caracteristica(id));
+
+create table celulares
+(id int not null primary key,
+artefactos_id int not null,
+caracteristicas_id int not null,
 descripcion varchar(100) not null,
 FOREIGN KEY (artefacto_id) REFERENCES artefacto(id),
 FOREIGN KEY (caracteristica_id) REFERENCES caracteristica(id));
@@ -17,10 +41,14 @@ modelo varchar(20) not null,
 signo_peso varchar(1) not null,
 precio int not null);
 
-insert into artefacto values (1, 'Cocina', 'marca1', 'modelo1', '$', '0');
-insert into artefacto values (2, 'Aire Acondicionado', 'marca1', 'modelo1', '$', '0');
-insert into artefacto values (3, 'Ventiladores', 'marca1', 'modelo1', '$', '0');
-insert into artefacto values (4, 'Estufa', 'marca1', 'modelo1', '$', '0');
+/*Electrodomesticos:*/
+insert into artefacto values (1, 'Cocina', 'mar', 'mod', '$', '0');
+insert into artefacto values (2, 'Aire Acondicionado', 'mar', 'mod', '$', '0');
+insert into artefacto values (3, 'Ventiladores', 'mar', 'mod', '$', '0');
+insert into artefacto values (4, 'Estufa', 'mar', 'mod', '$', '0');
+
+/*Televisores:*/
+insert into artefacto values (5, 'TV', 'mar', 'mod', '$', '0');
 
 create table caracteristica
 (id int not null primary key,
@@ -44,3 +72,41 @@ insert into caracteristica values (9, 'Consumo Energetico en Frio', null);
 insert into caracteristica values (10, 'Consumo Energetico en Calor', null);
 insert into caracteritica values (11, 'Medidas de unidad exterior', 'cm');
 insert into caracteritica values (11, 'Medidas de unidad interior', 'cm');
+
+/*Caracteristicas de Televisores:*/
+insert into caracteritica values (11, 'Panel', null);
+insert into caracteritica values (12, 'Tipo de Panel', null);
+insert into caracteritica values (13, 'Resolucion', null);
+insert into caracteritica values (14, 'Pulgadas de Pantalla', '´');
+insert into caracteritica values (15, 'SmartTV', null);
+insert into caracteritica values (16, 'Parlantes Sourround', null);
+
+/*Caracteristicas de Notebook:*/
+insert into caracteritica values (17, 'CPU', null);
+insert into caracteritica values (18, 'Modelo del Procesador', null);
+insert into caracteritica values (19, 'GPU', null);
+insert into caracteritica values (20, 'RAM', null);
+insert into caracteritica values (21, 'Tipo de RAM', null);
+insert into caracteritica values (22, 'Tipo de Disco', null);
+insert into caracteritica values (23, 'Capacidad de Disco', null);
+insert into caracteritica values (24, 'Tamaño de Pantalla', '´');
+insert into caracteritica values (25, 'Resolucion de Pantalla', null);
+insert into caracteritica values (26, 'Cantidad de Puertos USB', null);
+insert into caracteritica values (27, 'Puertos USB 2.0', null);
+insert into caracteritica values (28, 'Puertos USB 3.0', null);
+insert into caracteritica values (29, 'Unidad Lectora', null);
+insert into caracteritica values (30, 'Capacidad de Bateria', 'mAmp');
+
+/*Caracteristica de Celulares:*/
+insert into caracteritica values (31, 'Sistema Operativo', null);
+insert into caracteritica values (32, 'Procesador', null);
+insert into caracteritica values (33, 'Tamaño de Pantalla', '´');
+insert into caracteritica values (34, 'Tipo de Pantalla', null);
+insert into caracteritica values (35, 'Resolucion de Pantalla', null);
+insert into caracteritica values (36, 'RAM', null);
+insert into caracteritica values (37, 'Memoria Interna', null);
+insert into caracteritica values (38, 'Camara', 'PX');
+insert into caracteritica values (39, 'Posee Flash', null);
+insert into caracteritica values (40, 'Posee Camara Frontal', null);
+insert into caracteritica values (41, 'Camara Frontal', 'PX');
+insert into caracteritica values (42, 'Capacidad de Bateria', 'mAmp');
