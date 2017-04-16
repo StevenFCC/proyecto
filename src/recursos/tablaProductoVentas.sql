8417109984147ff9fc86c81dfa166ae1a9c19b9e
StevenFCC <valquiria98s@gmail.com>
2017-04-15 22:05:20 -0300

    SQL_2

--- src/recursos/tablaProductoVentas.sql
+++ src/recursos/tablaProductoVentas.sql
@@ -49,68 +49,78 @@ drop table caracteristicas;
 
 /*Artefactos*/
 /*insert into artefactos values (id, 'Tipo de Artefacto', 'Marca', 'Modelo', precio);*/
+
 /*Electrodomesticos:*/
-	/*Modelos copia*/
-    /*insert into artefactos values (4, 'Estufa', 'mar', 'mod', '0');*/
-	/*insert into artefactos values (5, 'Cafetera', 'mar', 'mod', '0');*/
+insert into artefactos values (1, 'Cocina', 'Whirlpool', 'WFX56EGDNA', 17599);
+insert into artefactos values (2, 'Cocina', 'Whirlpool', 'WF560XT', 20999);
+insert into artefactos values (3, 'Cocina', 'Whirlpool', 'WFX56DX', 12499);
+insert into artefactos values (4, 'Cocina', 'Patrick', 'CPF8251MVS', 7999);
+insert into artefactos values (5, 'Cocina', 'Patrick', 'CPF2251BVS', 7999);
+insert into artefactos values (6, 'Cocina', 'Patrick', 'CPF9651MVS', 8599);
+insert into artefactos values (7, 'Cocina', 'Domec', 'CTOBAV', 7880);
+insert into artefactos values (8, 'Cocina', 'Domec', 'CXUPV', 8499);
+insert into artefactos values (9, 'Cocina', 'Domec', 'CDXULEAV', 11499);
+insert into artefactos values (10, 'Cocina', 'Domec', 'CBUPV', 7499);
     
-    /*Modelos Reales*/
-	insert into artefactos values (1, 'Cocina', 'Whirlpool', 'WFX56EGDNA', 17599);
-    insert into artefactos values (2, 'Cocina', 'Whirlpool', 'WF560XT', 20999);
-    insert into artefactos values (3, 'Cocina', 'Whirlpool', 'WFX56DX', 12499);
-	insert into artefactos values (4, 'Cocina', 'Patrick', 'CPF8251MVS', 7999);
-    insert into artefactos values (5, 'Cocina', 'Patrick', 'CPF2251BVS', 7999);
-    insert into artefactos values (6, 'Cocina', 'Patrick', 'CPF9651MVS', 8599);
-    insert into artefactos values (7, 'Cocina', 'Domec', 'CTOBAV', 7880);
-    insert into artefactos values (8, 'Cocina', 'Domec', 'CXUPV', 8499);
-    insert into artefactos values (9, 'Cocina', 'Domec', 'CDXULEAV', 11499);
-    insert into artefactos values (10, 'Cocina', 'Domec', 'CBUPV', 7499);
+insert into artefactos values (11, 'Aire Acondicionado', 'Samsung', 'AR12KSFHCWK', 16499);
+insert into artefactos values (12, 'Aire Acondicionado', 'Samsung', 'AR18KSFHCWK', 22799);
+insert into artefactos values (13, 'Aire Acondicionado', 'LG', 'US-W246CSG4', 25999);
+insert into artefactos values (14, 'Aire Acondicionado', 'LG', 'US-W126BRG4', 19550);
+insert into artefactos values (15, 'Aire Acondicionado', 'LG', 'US-W246CRG4', 29999);
+insert into artefactos values (16, 'Aire Acondicionado', 'BGH', 'BC23FN', 10999);
+insert into artefactos values (17, 'Aire Acondicionado', 'BGH', 'BC30FN', 11999);
+insert into artefactos values (18, 'Aire Acondicionado', 'Sanyo', 'SA916ARN', 8899);
     
-    insert into artefactos values (11, 'Aire Acondicionado', 'Samsung', 'AR12KSFHCWK', 16499);
-    insert into artefactos values (12, 'Aire Acondicionado', 'Samsung', 'AR18KSFHCWK', 22799);
-    insert into artefactos values (13, 'Aire Acondicionado', 'LG', 'US-W246CSG4', 25999);
-    insert into artefactos values (14, 'Aire Acondicionado', 'LG', 'US-W126BRG4', 19550);
-    insert into artefactos values (15, 'Aire Acondicionado', 'LG', 'US-W246CRG4', 29999);
-    insert into artefactos values (16, 'Aire Acondicionado', 'BGH', 'BC23FN', 10999);
-    insert into artefactos values (17, 'Aire Acondicionado', 'BGH', 'BC30FN', 11999);
-    insert into artefactos values (18, 'Aire Acondicionado', 'Sanyo', 'SA916ARN', 8899);
-    
-    insert into artefactos values (19, 'Ventiladores', 'Protalia', 'V18P', 899);
-    insert into artefactos values (20, 'Ventiladores', 'Sansei', 'VPS1030', 799);
+insert into artefactos values (19, 'Ventiladores', 'Protalia', 'V18P', 899);
+insert into artefactos values (20, 'Ventiladores', 'Sansei', 'VPS1030', 799);
     
     
 
 /*Televisores:*/
-	/*Modelos Reales*/
-	insert into artefactos values (21, 'TV', 'Samsung', 'UN32J4000AGCTC', 5999);
-    insert into artefactos values (22, 'TV', 'Samsung', 'UN40J5300AGCDF', 11999);
-    insert into artefactos values (23, 'TV', 'Samsung', 'UN50J5300AGCDF', 15999);
-    insert into artefactos values (24, 'TV', 'LG', '43UH6500', 13999);
-    insert into artefactos values (25, 'TV', 'LG', '32LH575B', 7999);
-    insert into artefactos values (26, 'TV', 'LG', '65UG8700.AWG', 49999);
-    insert into artefactos values (27, 'TV', 'LG', '49LH5700', 15999);
+insert into artefactos values (21, 'TV', 'Samsung', 'UN32J4000AGCTC', 5999);
+insert into artefactos values (22, 'TV', 'Samsung', 'UN40J5300AGCDF', 11999);
+insert into artefactos values (23, 'TV', 'Samsung', 'UN50J5300AGCDF', 15999);
+insert into artefactos values (24, 'TV', 'LG', '43UH6500', 13999);
+insert into artefactos values (25, 'TV', 'LG', '32LH575B', 7999);
+insert into artefactos values (26, 'TV', 'LG', '65UG8700.AWG', 49999);
+insert into artefactos values (27, 'TV', 'LG', '49LH5700', 15999);
     
 /*Notebook*/
-	/*Modelos copia*/
-	insert into artefactos values (28, 'Notebook', '', '', 0);
-
-    /*Modelos Reales*/
-    insert into artefactos values (28, 'Notebook', 'ASUS', 'X451UA-GO835T', 13999);
-    insert into artefactos values (28, 'Notebook', 'ASUS', 'X555LD-XX027H', 15699);
-    insert into artefactos values (28, 'Notebook', 'ASUS', 'X451UA-GO835T', 13999);
-    insert into artefactos values (28, 'Notebook', 'HP', '15-AY029LA', 15999);
-    insert into artefactos values (28, 'Notebook', 'HP', '14-AM092LA', 12999);
-    insert into artefactos values (28, 'Notebook', 'HP', '15-AY013LA', 17999);
-    insert into artefactos values (28, 'Notebook', 'HP', '15-AY029LA', 12999);
-    insert into artefactos values (28, 'Notebook', 'EXO', 'WINGS K2200', 5499);
-    insert into artefactos values (28, 'Notebook', 'EXO', 'R9XF1445', 5999);
+insert into artefactos values (28, 'Notebook', 'ASUS', 'X451UA-GO835T', 13999);
+insert into artefactos values (29, 'Notebook', 'ASUS', 'X555LD-XX027H', 15699);
+insert into artefactos values (30, 'Notebook', 'ASUS', 'X451UA-GO835T', 13999);
+insert into artefactos values (31, 'Notebook', 'HP', '15-AY029LA', 15999);
+insert into artefactos values (32, 'Notebook', 'HP', '14-AM092LA', 12999);
+insert into artefactos values (33, 'Notebook', 'HP', '15-AY013LA', 17999);
+insert into artefactos values (34, 'Notebook', 'HP', '15-AY029LA', 12999);
+insert into artefactos values (35, 'Notebook', 'EXO', 'WINGS K2200', 5499);
+insert into artefactos values (36, 'Notebook', 'EXO', 'R9XF1445', 5999);
 
 /*Smartphone*/
-	/*Modelos copia*/
-	insert into artefactos values (8, 'Smartphone', 'mar', 'mod', '0');
-
-    /*Modelos Reales*/
-    
+insert into artefactos values (38, 'Smartphone', 'SONY', 'XPERIA XA', 7999);
+insert into artefactos values (39, 'Smartphone', 'SONY', 'XPERIA M5', 10699);
+insert into artefactos values (40, 'Smartphone', 'SONY', 'XPERIA M4', 5999);
+insert into artefactos values (41, 'Smartphone', 'Motorola', 'MOTO G4 PLAY', 5499);
+insert into artefactos values (42, 'Smartphone', 'Motorola', 'MOTO G4', 6999);
+insert into artefactos values (43, 'Smartphone', 'Samsung', 'GALAXY J5', 6999);
+insert into artefactos values (44, 'Smartphone', 'Samsung', 'GALAXY J1', 3499);
+insert into artefactos values (45, 'Smartphone', 'Samsung', 'GALAXY J7', 7999);
+insert into artefactos values (46, 'Smartphone', 'Samsung', 'GALAXY J2 PRIME', 3999);
+insert into artefactos values (47, 'Smartphone', 'Samsung', 'GALAXY S7 EDGE', 22999);
+insert into artefactos values (48, 'Smartphone', 'Samsung', 'GALAXY S6 EDGE', 15999);
+insert into artefactos values (49, 'Smartphone', 'Samsung', 'NOTE 5', 15999);
+insert into artefactos values (50, 'Smartphone', 'Samsung', 'GALAXY GRAND PRIME', 5999);
+insert into artefactos values (51, 'Smartphone', 'Samsung', 'GALAXY A5', 9999);
+insert into artefactos values (52, 'Smartphone', 'Samsung', 'GALAXY A3', 6499);
+insert into artefactos values (53, 'Smartphone', 'Apple', 'iPhone 7', 27999);
+insert into artefactos values (54, 'Smartphone', 'Apple', 'iPhone SE', 16999);
+insert into artefactos values (55, 'Smartphone', 'Lenovo', 'VIBE K5', 3999);
+insert into artefactos values (56, 'Smartphone', 'Lenovo', 'K4', 2999);
+insert into artefactos values (57, 'Smartphone', 'Huawei', 'P8', 6499);
+insert into artefactos values (58, 'Smartphone', 'Huawei', 'Y550', 2999);
+insert into artefactos values (59, 'Smartphone', 'Alcatel', 'PIXI 3', 1799);
+insert into artefactos values (60, 'Smartphone', 'Philips', 'S616', 5899);
+   
     
 create table caracteristicas
 (id int not null primary key,
@@ -118,59 +128,68 @@ nombre varchar(30) not null,
 unidad_de_medida varchar(20));
 
 /*Caracteristicas Generales:*/
-insert into caracteristicas values (1, 'Consumo Energetico', 'Watts');
-insert into caracteristicas values (2, 'Eficiencia Enrgetica', null);
-insert into caracteristicas values (3, 'Medidas', 'cm');
-insert into caracteristicas values (4, 'Descripcion', null);
+/*insert into caracteristicas values ('id (Dado por autoincremento)', 'Caracteristica', 'Unidad de Medida (Si posee)')*/
+insert into caracteristicas values ('Consumo Energetico', 'Watts');
+insert into caracteristicas values ('Medidas', 'cm');
+insert into caracteristicas values ('Descripcion 1', null);
+insert into caracteristicas values ('Descripcion 2', null);
+insert into caracteristicas values ('Descripcion 3', null);
 
 /*Caracteristicas de Cocinas:*/
-insert into caracteristicas values (5, 'Cantidad de hornallas', null);
-insert into caracteristicas values (5, 'Fuente', null);
-insert into caracteristicas values (1, 'Consumo Energetico', 'Watts');
-insert into caracteristicas values (2, 'Eficiencia Enrgetica', null);
-insert into caracteristicas values (2, 'Consumo de Gas', null);
+insert into caracteristicas values ('Eficiencia Enrgetica', null);
+insert into caracteristicas values ('Cantidad de hornallas', null);
+insert into caracteristicas values ('Tipo de Fuente', null);
+insert into caracteristicas values ('Consumo Energetico', 'Watts');
+insert into caracteristicas values ('Eficiencia Enrgetica', null);
+insert into caracteristicas values ('Consumo de Gas', null);
 
 /*Caracteristicas de Aires Acondicionados:*/
-insert into caracteristicas values (6, 'Frio', null);
-insert into caracteristicas values (7, 'Calor', null);
-insert into caracteristicas values (8, 'Tipo de Aire', null);
-insert into caracteristicas values (9, 'Consumo Energetico en Frio', null);
-insert into caracteristicas values (10, 'Consumo Energetico en Calor', null);
-insert into caracteristicas values (11, 'Medidas de unidad exterior', 'cm');
-insert into caracteristicas values (12, 'Medidas de unidad interior', 'cm');
+insert into caracteristicas values ('Eficiencia Enrgetica', null);
+insert into caracteristicas values ('Frio', null);
+insert into caracteristicas values ('Calor', null);
+insert into caracteristicas values ('Tipo de Aire', null);
+insert into caracteristicas values ('Consumo Energetico en Frio', null);
+insert into caracteristicas values ('Consumo Energetico en Calor', null);
+insert into caracteristicas values ('Medidas de unidad exterior', 'cm');
+insert into caracteristicas values ('Medidas de unidad interior', 'cm');
+
+/*Caracteristicas de Ventiladores:*/
+insert into caracteristicas values ('Numero de Velocidades', null);
 
 /*Caracteristicas de Televisores:*/
-insert into caracteristicas values (13, 'Resolucion', null);
-insert into caracteristicas values (14, 'Pulgadas de Pantalla', '´');
-insert into caracteristicas values (15, 'SmartTV', null);
-insert into caracteristicas values (16, 'Parlantes Sourround', null);
+insert into caracteristicas values ('Panel', null);
+insert into caracteristicas values ('Eficiencia Enrgetica', null);
+insert into caracteristicas values ('Resolucion', null);
+insert into caracteristicas values ('Pulgadas de Pantalla', '´');
+insert into caracteristicas values ('SmartTV', null);
+insert into caracteristicas values ('Parlantes Sourround', null);
 
 /*Caracteristicas de Notebook:*/
-insert into caracteristicas values (17, 'CPU', null);
-insert into caracteristicas values (18, 'Modelo del Procesador', null);
-insert into caracteristicas values (19, 'GPU', null);
-insert into caracteristicas values (20, 'RAM', null);
-insert into caracteristicas values (21, 'Tipo de RAM', null);
-insert into caracteristicas values (22, 'Tipo de Disco', null);
-insert into caracteristicas values (23, 'Capacidad de Disco', null);
-insert into caracteristicas values (24, 'Tamaño de Pantalla', '´');
-insert into caracteristicas values (25, 'Resolucion de Pantalla', null);
-insert into caracteristicas values (26, 'Cantidad de Puertos USB', null);
-insert into caracteristicas values (27, 'Puertos USB 2.0', null);
-insert into caracteristicas values (28, 'Puertos USB 3.0', null);
-insert into caracteristicas values (29, 'Unidad Lectora', null);
-insert into caracteristicas values (30, 'Capacidad de Bateria', 'mAmp');
+insert into caracteristicas values ('CPU', null);
+insert into caracteristicas values ('Modelo del Procesador', null);
+insert into caracteristicas values ('GPU', null);
+insert into caracteristicas values ('RAM', null);
+insert into caracteristicas values ('Tipo de RAM', null);
+insert into caracteristicas values ('Tipo de Disco', null);
+insert into caracteristicas values ('Capacidad de Disco', null);
+insert into caracteristicas values ('Tamaño de Pantalla', '´');
+insert into caracteristicas values ('Resolucion de Pantalla', null);
+insert into caracteristicas values ('Cantidad de Puertos USB', null);
+insert into caracteristicas values ('Puertos USB 2.0', null);
+insert into caracteristicas values ('Puertos USB 3.0', null);
+insert into caracteristicas values ('Unidad Lectora', null);
+insert into caracteristicas values ('Capacidad de Bateria', 'mAmp');
 
 /*Caracteristica de Smartphone:*/
-insert into caracteristicas values (31, 'Sistema Operativo', null);
-insert into caracteristicas values (32, 'Procesador', null);
-insert into caracteristicas values (33, 'Tamaño de Pantalla', '´');
-insert into caracteristicas values (34, 'Tipo de Pantalla', null);
-insert into caracteristicas values (35, 'Resolucion de Pantalla', null);
-insert into caracteristicas values (36, 'RAM', null);
-insert into caracteristicas values (37, 'Memoria Interna', null);
-insert into caracteristicas values (38, 'Camara', 'PX');
-insert into caracteristicas values (39, 'Posee Flash', null);
-insert into caracteristicas values (40, 'Posee Camara Frontal', null);
-insert into caracteristicas values (41, 'Camara Frontal', 'PX');
-insert into caracteristicas values (42, 'Capacidad de Bateria', 'mAmp');
+insert into caracteristicas values ('Sistema Operativo', null);
+insert into caracteristicas values ('Procesador', null);
+insert into caracteristicas values ('Tamaño de Pantalla', '´');
+insert into caracteristicas values ('Tipo de Pantalla', null);
+insert into caracteristicas values ('Resolucion de Pantalla', null);
+insert into caracteristicas values ('RAM', null);
+insert into caracteristicas values ('Memoria Interna', null);
+insert into caracteristicas values ('Camara', 'PX');
+insert into caracteristicas values ('Posee Flash', null);
+insert into caracteristicas values ('Posee Camara Frontal', null);
+insert into caracteristicas values ('Camara Frontal', 'PX');
+insert into caracteristicas values ('Capacidad de Bateria', 'mAmp');
