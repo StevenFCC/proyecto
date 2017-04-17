dfe8bed0bdc85cda22557e7c97288724013f9796
Steven Fernandez <valquiria98s@gmail.com>
2017-04-17 09:41:48 -0300

    JAVA_4

--- src/main/java/org/proyecto/entities/Electrodomesticos/AireAcondicionado.java
+++ src/main/java/org/proyecto/entities/Electrodomesticos/AireAcondicionado.java
@@ -1,5 +1,12 @@
 package org.proyecto.entities.Electrodomesticos;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import org.proyecto.connection.JDBCConnection;
+
 public class AireAcondicionado extends Electrodomestico{
 	private boolean frio;
 	private boolean calor;
@@ -8,4 +15,41 @@ public class AireAcondicionado extends Electrodomestico{
 	private String consumoEnergeticoEnCalor;
 	private String medidasDeUnidadExterior;
 	private String medidasDeUnidadInterior;
+	
+	
+	public boolean isFrio() {
+		return frio;
+	}
+	
+	public boolean isCalor() {
+		return calor;
+	}
+	
+	public String getTipoDeAire() {
+		return tipoDeAire;
+	}
+	
+	public String getConsumoEnergeticoEnFrio() {
+		return consumoEnergeticoEnFrio;
+	}
+	
+	public String getConsumoEnergeticoEnCalor() {
+		return consumoEnergeticoEnCalor;
+	}
+	
+	public String getMedidasDeUnidadExterior() {
+		return medidasDeUnidadExterior;
+	}
+	
+	public String getMedidasDeUnidadInterior() {
+		return medidasDeUnidadInterior;
+	}
+	
+	public AireAcondicionado(int idAireAcondicionado) throws SQLException {
+		Connection con = JDBCConnection.connectToDataBase();
+		Statement st = con.createStatement();
+		ResultSet rs = st.executeQuery("select * from electrodomesticos where id = " + idAireAcondicionado);
+		this.consumoEnergetico = rs.getInt(1);
+		
+	}
 }
--- src/main/java/org/proyecto/entities/Electrodomesticos/Electrodomestico.java
+++ src/main/java/org/proyecto/entities/Electrodomesticos/Electrodomestico.java
@@ -5,4 +5,17 @@ public abstract class Electrodomestico {
 	protected int consumoEnergetico;
 	protected String medidas;
 	protected String eficienciaEnergetica;
+	
+	
+	public int getConsumoEnergetico() {
+		return consumoEnergetico;
+	}
+	
+	public String getMedidas() {
+		return medidas;
+	}
+	
+	public String getEficienciaEnergetica() {
+		return eficienciaEnergetica;
+	}	
 }
