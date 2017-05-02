76694b48a39453135f6f1eba566eb164cbf2b258
StevenFCC <valquiria98s@gmail.com>
2017-05-01 23:25:27 -0300

    01/05/23:25

--- src/recursos/tablaUsuariosYRegistrosDeCompras.sql
+++ src/recursos/tablaUsuariosYRegistrosDeCompras.sql
@@ -12,11 +12,12 @@ nombre_de_administrador varchar(50) not null,
 clave varchar (20) not null);
 
 create table registroDeCompras
-(id int not null,
+(id int not null primary key auto_increment,
 usuario_que_realizo_la_compra varchar(50) not null,
 producto_comprado varchar(50) not null);
 
 drop table usuarios;
 drop table usuariosadministradores;
+drop table registrodecompras;
 
 
